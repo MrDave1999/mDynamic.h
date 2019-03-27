eb9c31d745e1f2408aff7b66ddfa1669fcb4d83e
David Román <43916038+MrDave1999@users.noreply.github.com>
2019-03-27 12:03:18 -0500

    Header C

--- include/mDynamic.h
+++ include/mDynamic.h
@@ -1,17 +1,30 @@
 /*
-			mDynamic.h - Matrix Dynamic.
+			mDynamic.h - Matrix Dynamic. Versión [2.0].
 			Copyright (c) David Román. All rights reserved.
 */
 
 #ifndef _INC_MDYNAMIC
 #define _INC_MDYNAMIC
 
-#include <stdio.h> /* for puts */
-#include <stdlib.h> /* for malloc, realloc and free */
+#include <stdint.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 
-#ifndef FREE_MA_FUNCTION
-	#error You must define the macro FREE_MA_FUNCTION
+struct _dim
+{
+	int32_t new_rows;
+	int32_t old_rows;
+	int32_t cols;
+};
+
+#ifndef MAX_MATRIX
+	#error You must define the macro MAX_MATRIX.
 #endif
+
+struct _dim dim[MAX_MATRIX];
+void* ptrtemp;
+uint8_t id;
+uint32_t it; 
 
 #ifndef MESSAGE_ALLOC_ROW
 	#define MESSAGE_ALLOC_ROW	"Error allocating memory for the rows"
@@ -29,98 +42,106 @@
 	#define MESSAGE_REALLOC_COL "Error (re)allocating memory for the columns"
 #endif
 
-#define EXISTS_MATRIX(nameptr, rows) if (nameptr != NULL) { DELETE_MATRIX(nameptr, rows) }
-#define DERROR() !!(error != 0) /* Detect error */ 
+#define getrows(matrixid) dim[matrixid].new_rows
+#define getcols(matrixid) dim[matrixid].cols
 
-void* ptrtemp = NULL;
-unsigned int error;
-unsigned int it; /* count iterations in loops/bucles */
+#define setrows(matrixid, value) dim[matrixid].new_rows = value;
+#define setcols(matrixid, value) dim[matrixid].cols = value;
 
-#define DELETE_MATRIX(nameptr, rows)													\
-for (it = 0; it != rows; ++it)															\
-	free(*(nameptr + it));																\
-free(nameptr);																			\
+#define getdata(matrixid, namearray, nameptr, numrow, numcol) (*(*(namearray[matrixid].nameptr + numrow) + numcol))
+#define getadd(matrixid, namearray, nameptr, numrow) (*(namearray[matrixid].nameptr + numrow))
+#define setdata(matrixid, namearray, nameptr, numrow, numcol, value) getdata(matrixid, namearray, nameptr, numrow, numcol) = value;
+
+#define DELETE_MATRIX(nameptr, rows)																													\
+for (it = 0; it != rows; ++it)																															\
+	free(*(nameptr + it));																																\
+free(nameptr);																																			\
 nameptr = NULL;
 
 
-#define CREATE_MATRIX(typedata, nameptr, rows, cols)									\
-error = 0;																				\
-nameptr = (typedata**)malloc(rows * sizeof nameptr);									\
-if (nameptr == NULL)																	\
-{																						\
-	puts(""MESSAGE_ALLOC_ROW"");														\
-	error = 1;																			\
-}																						\
-else																					\
-{																						\
-	for (it = 0; it != rows; ++it)														\
-	{																					\
-		*(nameptr + it) = (typedata*)malloc(cols * sizeof **nameptr);					\
-		if (*(nameptr + it) == NULL)													\
-		{																				\
-			puts(""MESSAGE_ALLOC_COL"");												\
-			error = 1;																	\
-			rows = it;																	\
-			DELETE_MATRIX(nameptr, rows)												\
-			break;																		\
-		}																				\
-	}																					\
-}if(FREE_MA_FUNCTION == NULL) return NULL;
+#define FREE_MEMORY_ALL(namearray, nameptr)																												\
+for (id = 0; id != MAX_MATRIX; ++id)																													\
+{																																						\
+	if (namearray[id].nameptr != NULL)																													\
+	{																																					\
+		DELETE_MATRIX(namearray[id].nameptr, dim[id].new_rows)																							\
+	}																																					\
+}
 
 
-#define REALLOC_COLS(typedata, nameptr, rows, newcols)									\
-error = 0;																				\
-for (it = 0; it != rows; ++it)															\
-{																						\
-	ptrtemp = *(nameptr + it);															\
-	*(nameptr + it) = (typedata*)realloc(*(nameptr + it), newcols * sizeof **nameptr);	\
-	if (*(nameptr + it) == NULL)														\
-	{																					\
-		*(nameptr + it) = (typedata*)ptrtemp;											\
-		puts(""MESSAGE_REALLOC_COL"");													\
-		error = 1;																		\
-		DELETE_MATRIX(nameptr, rows)													\
-		break;																			\
-	}																					\
-}if(FREE_MA_FUNCTION == NULL) return NULL;																								
+#define ALLOC_RC(matrixid, namearray, nameptr)																											\
+namearray[matrixid].nameptr = malloc(dim[matrixid].new_rows * sizeof namearray[matrixid].nameptr);														\
+if (namearray[matrixid].nameptr == NULL)																												\
+{																																						\
+	puts(""MESSAGE_ALLOC_ROW"");																														\
+	FREE_MEMORY_ALL(namearray, nameptr)																													\
+	return EXIT_FAILURE;																																\
+}																																						\
+for (it = 0; it != dim[matrixid].new_rows; ++it)																										\
+{																																						\
+	*(namearray[matrixid].nameptr + it) = malloc(dim[matrixid].cols * sizeof **namearray[matrixid].nameptr);											\
+	if (*(namearray[matrixid].nameptr + it) == NULL)																									\
+	{																																					\
+		puts(""MESSAGE_ALLOC_COL"");																													\
+		dim[matrixid].new_rows = it;																													\
+		DELETE_MATRIX(namearray[matrixid].nameptr, dim[matrixid].new_rows)																				\
+		FREE_MEMORY_ALL(namearray, nameptr)																												\
+		return EXIT_FAILURE;																															\
+	}																																					\
+}																																						\
+dim[matrixid].old_rows = dim[matrixid].new_rows;
 
 
-#define REALLOC_ROWS(typedata, nameptr, new_rows, old_rows, cols)						\
-error = 0;																				\
-if (new_rows < old_rows)																\
-{																						\
-	for (it = new_rows; it != old_rows; ++it)											\
-		free(*(nameptr + it));															\
-}																						\
-ptrtemp = (void**)nameptr;																\
-nameptr = (typedata**)realloc(nameptr, new_rows * sizeof nameptr);						\
-if (nameptr == NULL)																	\
-{																						\
-	puts(""MESSAGE_REALLOC_ROW"");														\
-	error = 1;																			\
-	if(ptrtemp != NULL)																	\
-	{																					\
-		nameptr = (typedata**)ptrtemp;													\
-		DELETE_MATRIX(nameptr, old_rows)												\
-	}																					\
-}																						\
-else																					\
-{																						\
-	if (new_rows > old_rows)															\
-	{																					\
-		for (it = old_rows; it != new_rows; ++it)										\
-		{																				\
-			*(nameptr + it) = (typedata*)malloc(cols * sizeof **nameptr);				\
-			if (*(nameptr + it) == NULL)												\
-			{																			\
-				puts(""MESSAGE_ALLOC_COL"");											\
-				new_rows = it;															\
-				error = 1;																\
-				DELETE_MATRIX(nameptr, new_rows)										\
-				break;																	\
-			}																			\
-		}																				\
-	}																					\
-}if(FREE_MA_FUNCTION == NULL) return NULL;
+#define REALLOC_COLS(matrixid, namearray, nameptr)																										\
+for (it = 0; it != dim[matrixid].new_rows; ++it)																										\
+{																																						\
+	ptrtemp = *(namearray[matrixid].nameptr + it);																										\
+	*(namearray[matrixid].nameptr + it) = realloc(*(namearray[matrixid].nameptr + it), dim[matrixid].cols * sizeof **namearray[matrixid].nameptr);		\
+	if (*(namearray[matrixid].nameptr + it) == NULL)																									\
+	{																																					\
+		*(namearray[matrixid].nameptr + it) = ptrtemp;																									\
+		puts(""MESSAGE_REALLOC_COL"");																													\
+		DELETE_MATRIX(namearray[matrixid].nameptr, dim[matrixid].new_rows)																				\
+		FREE_MEMORY_ALL(namearray, nameptr)																												\
+		return EXIT_FAILURE;																															\
+	}																																					\
+}																						
+
+
+#define REALLOC_ROWS(matrixid, namearray, nameptr)																										\
+if (dim[matrixid].new_rows < dim[matrixid].old_rows)																									\
+{																																						\
+	for (it = dim[matrixid].new_rows; it != dim[matrixid].old_rows; ++it)																				\
+		free(*(namearray[matrixid].nameptr + it));																										\
+}																																						\
+ptrtemp = namearray[matrixid].nameptr;																													\
+namearray[matrixid].nameptr = realloc(namearray[matrixid].nameptr, dim[matrixid].new_rows * sizeof namearray[matrixid].nameptr);						\
+if (namearray[matrixid].nameptr == NULL)																												\
+{																																						\
+	puts(""MESSAGE_REALLOC_ROW"");																														\
+	if(ptrtemp != NULL)																																	\
+	{																																					\
+		namearray[matrixid].nameptr = ptrtemp;																											\
+		DELETE_MATRIX(namearray[matrixid].nameptr, dim[matrixid].old_rows)																				\
+	}																																					\
+	FREE_MEMORY_ALL(namearray, nameptr)																													\
+	return EXIT_FAILURE;																																\
+}																																						\
+if (dim[matrixid].new_rows > dim[matrixid].old_rows)																									\
+{																																						\
+	for (it = dim[matrixid].old_rows; it != dim[matrixid].new_rows; ++it)																				\
+	{																																					\
+		*(namearray[matrixid].nameptr + it) = malloc(dim[matrixid].cols * sizeof **namearray[matrixid].nameptr);										\
+		if (*(namearray[matrixid].nameptr + it) == NULL)																								\
+		{																																				\
+			puts(""MESSAGE_ALLOC_COL"");																												\
+			dim[matrixid].new_rows = it;																												\
+			DELETE_MATRIX(namearray[matrixid].nameptr, dim[matrixid].new_rows)																			\
+			FREE_MEMORY_ALL(namearray, nameptr)																											\
+			return EXIT_FAILURE;																														\
+		}																																				\
+	}																																					\
+}																																						\
+dim[matrixid].old_rows = dim[matrixid].new_rows;
 
 #endif /* _INC_MDYNAMIC */
