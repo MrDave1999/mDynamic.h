23a3e80fef14755563f2959f156a52b709bf7bc4
David Román <43916038+MrDave1999@users.noreply.github.com>
2019-04-08 13:14:59 -0500

    Source C

--- Programa/Programa.c
+++ Programa/Programa.c
@@ -2,7 +2,6 @@
 	El siguiente ejemplo se basa en tres matrices:
 	- Las dimensiones y los datos de la matriz A deben ser pedidos por el usuario y se debe
 	calcular el promedio de todos los elementos de dicha matriz.
-
 	- Las dimensiones y los datos de la matriz B y C deben ser calculados de forma aleatoria y
 	se debe calcular la suma total de sus elementos de cada matriz. La matriz B y C son de tipo "int".
 	Además de eso cada vez que se muestre el resultado, se debe mostrar después de eso al usuario
@@ -14,22 +13,14 @@
 #include <pscanf.h>
 
 #define MAX_MATRIX 3
-#include <mDynamic.h>
+#include "mDynamic.h"
 
 enum ID_MATRIZ
-{ 
+{
 	MA, //Matriz A
 	MB, //Matriz B
 	MC  //Matriz C
 };
-
-union pun
-{
-	float** pa; //Puntero doble para la matriz A 
-	int** pbc; //Puntero doble para la matriz B y C
-};
-
-union pun matriz[MAX_MATRIX];
 
 uint8_t CrearMatrizA(void)
 {
@@ -46,12 +37,12 @@ uint8_t CrearMatrizA(void)
 		assertc(getcols(MA) >= 1, "Error: El numero de columnas debe ser mayor a 0.")
 		break;
 	} while (1);
-	ALLOC_RC(MA, matriz, pa)
+	alloc_m(float, MA);
 	for (i = 0; i != getrows(MA); ++i)
 	{
 		for (j = 0; j != getcols(MA); ++j)
 		{ //Aquí si es necesario las llaves, la macro "dataread" expande tres códigos 
-			dataread("%f", &matriz[MA].pa[i][j], "Ingrese un dato en la fila: %d con respecto a la columna: %d", i+1, j+1);
+			dataread("%f", &gd(float, MA, i, j), "Ingrese un dato en la fila: %d con respecto a la columna: %d", i + 1, j + 1);
 		}
 	}
 	return EXIT_SUCCESS;
@@ -64,7 +55,7 @@ float CalcularPromedio_MA(void)
 	for (i = 0; i != getrows(MA); ++i)
 	{
 		for (j = 0; j != getcols(MA); ++j)
-			suma += matriz[MA].pa[i][j];
+			suma += gd(float, MA, i, j);
 	}
 	return suma / (getrows(MA)* getcols(MA));
 }
@@ -73,23 +64,21 @@ uint8_t CrearMatrizBC(const uint8_t matriz_id)
 {
 	uint32_t i, j;
 	int32_t suma = 0;
-	setrows(matriz_id, randomEx(1, 5))
-	setcols(matriz_id, randomEx(1, 5))
-	REALLOC_ROWS(matriz_id, matriz, pbc)
-	REALLOC_COLS(matriz_id, matriz, pbc)
-
+	setrows(matriz_id, randomEx(1, 5));
+	setcols(matriz_id, randomEx(1, 5));
+	realloc_m(int, matriz_id, ROWS_COLS);
 	for (i = 0; i != getrows(matriz_id); ++i)
 	{
 		for (j = 0; j != getcols(matriz_id); ++j)
-			matriz[matriz_id].pbc[i][j] = random(10);
+			sd(int, matriz_id, i, j, random(10));
 	}
 	printf("\nMatriz %c (%d x %d)\n\n", (matriz_id == MB) ? 'B' : 'C', getrows(matriz_id), getcols(matriz_id));
 	for (i = 0; i != getrows(matriz_id); ++i)
 	{
 		for (j = 0; j != getcols(matriz_id); ++j)
 		{
-			printf("%d\t", matriz[matriz_id].pbc[i][j]);
-			suma += matriz[matriz_id].pbc[i][j];
+			printf("%d\t", gd(int, matriz_id, i, j));
+			suma += gd(int, matriz_id, i, j);
 		}
 		puts("");
 	}
@@ -101,11 +90,7 @@ int main(void)
 {
 	char opcion = -1;
 	//Si hubo algún error al momento de reservar memoria para la matriz A, el programa termina.
-	if (CrearMatrizA() == EXIT_FAILURE)
-	{
-		pauseprogram();
-		return EXIT_FAILURE;
-	}
+	error(CrearMatrizA);
 	printf("El promedio de elementos de la matriz A fue de %f\n\n", CalcularPromedio_MA());
 	pauseprogram();
 	while (1)
@@ -114,19 +99,12 @@ int main(void)
 		{
 			dataread("%c", &opcion, "Deseas seguir re-dimensionando? Ingresa S/N - S de SI/N de NO");
 			assertc(opcion == 'S' || opcion == 'N', "Error: Debes ingresar S o N\n")
-			if (opcion == 'N')
-			{
-				FREE_MEMORY_ALL(matriz, pa)/* Hay que liberar la memoria de las matrices activas en el heap */
-				return EXIT_SUCCESS;
-			}
+			if (opcion == 'N') break;
 		}
-		if (CrearMatrizBC(MB) == EXIT_FAILURE || CrearMatrizBC(MC) == EXIT_FAILURE)
-		{
-			pauseprogram();
-			return EXIT_FAILURE;
-		}
+		error(CrearMatrizBC, MB);
+		error(CrearMatrizBC, MC);
 		pauseprogram();
 		opcion = -2;
 	}
 	return EXIT_SUCCESS;
-}   
+}
