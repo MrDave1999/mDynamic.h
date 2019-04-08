0b50841becfaaa5294ed77f0d2f007bdfa30d2d7
David Román <43916038+MrDave1999@users.noreply.github.com>
2019-04-08 13:18:12 -0500

    Header C

--- include/mDynamic.h
+++ include/mDynamic.h
@@ -1,5 +1,5 @@
 /*
-			mDynamic.h - Matrix Dynamic. Versión [2.0].
+			mDynamic.h - Matrix Dynamic. Versión [3.0]
 			Copyright (c) David Román. All rights reserved.
 */
 
@@ -9,22 +9,6 @@
 #include <stdint.h>
 #include <stdio.h> 
 #include <stdlib.h> 
-
-struct _dim
-{
-	int32_t new_rows;
-	int32_t old_rows;
-	int32_t cols;
-};
-
-#ifndef MAX_MATRIX
-	#error You must define the macro MAX_MATRIX.
-#endif
-
-struct _dim dim[MAX_MATRIX];
-void* ptrtemp;
-uint8_t id;
-uint32_t it; 
 
 #ifndef MESSAGE_ALLOC_ROW
 	#define MESSAGE_ALLOC_ROW	"Error allocating memory for the rows"
@@ -42,106 +26,198 @@ uint32_t it;
 	#define MESSAGE_REALLOC_COL "Error (re)allocating memory for the columns"
 #endif
 
+#ifndef MAX_MATRIX 
+	#error You must define the macro MAX_MATRIX.
+#endif
+
+/* Tipo de dimensión o el modo de re-dimensionar */
+typedef enum
+{
+	ROWS,
+	COLS,
+	ROWS_COLS
+}_mode;
+
+/* Almacenamiento de dimensiones de cada matriz */
+struct _dim
+{
+	int32_t new_rows;
+	int32_t old_rows;
+	int32_t cols;
+}dim[MAX_MATRIX];
+
+void** buf_ptr[MAX_MATRIX]; /* Búfer de punteros dobles */
+
+/* Modifica/accede a un respectivo elemento de un búfer de punteros */
+#define MOD_BUF_PTR (*(*(buf_ptr + matrixid) + i))
+
+/* Obtiene la cantidad de filas y columnas de una matriz */
 #define getrows(matrixid) dim[matrixid].new_rows
 #define getcols(matrixid) dim[matrixid].cols
 
-#define setrows(matrixid, value) dim[matrixid].new_rows = value;
-#define setcols(matrixid, value) dim[matrixid].cols = value;
+/* Asigna un número de filas y columnas para una matriz  */
+#define setrows(matrixid, value) dim[matrixid].new_rows = value
+#define setcols(matrixid, value) dim[matrixid].cols = value
 
-#define getdata(matrixid, namearray, nameptr, numrow, numcol) (*(*(namearray[matrixid].nameptr + numrow) + numcol))
-#define getadd(matrixid, namearray, nameptr, numrow) (*(namearray[matrixid].nameptr + numrow))
-#define setdata(matrixid, namearray, nameptr, numrow, numcol, value) getdata(matrixid, namearray, nameptr, numrow, numcol) = value;
+/* gd = getdata -> Obtiene un dato de una respectiva columna */
+#define gd(typedata, matrixid, i, j) (*((typedata*)*(*(buf_ptr + matrixid) + i) + j))
 
-#define DELETE_MATRIX(nameptr, rows)																													\
-for (it = 0; it != rows; ++it)																															\
-	free(*(nameptr + it));																																\
-free(nameptr);																																			\
-nameptr = NULL;
+/* ga = getaddress -> Obtiene la dirección de memoria del primer elemento de cualquier columna */
+#define ga(matrixid, i) (*(*(buf_ptr + matrixid) + i))
 
+/* sd = setdata -> Asigna un dato a una respectiva columna de una fila */
+#define sd(typedata, matrixid, i, j, value) gd(typedata, matrixid, i, j) = value
 
-#define FREE_MEMORY_ALL(namearray, nameptr)																												\
-for (id = 0; id != MAX_MATRIX; ++id)																													\
-{																																						\
-	if (namearray[id].nameptr != NULL)																													\
-	{																																					\
-		DELETE_MATRIX(namearray[id].nameptr, dim[id].new_rows)																							\
-	}																																					\
+/* Detecta si la matriz fue re-dimensionada o no */
+#define RE_ASSIGN							\
+for(i = 0; i != MAX_MATRIX; ++i)			\
+	if (dim[i].new_rows != dim[i].old_rows) \
+		dim[i].new_rows = dim[i].old_rows
+
+#ifndef error
+/* Detecta si una función personalizada tuvo algún error durante la asignación de memoria */
+	#define error(namefunc, ...) if(namefunc(__VA_ARGS__) == EXIT_FAILURE) return EXIT_FAILURE
+#endif
+
+/* Macro para la asignación de memoria */
+#define alloc_m(typedata, matrixid) if(_alloc_m(matrixid, sizeof(typedata)) == NULL) return EXIT_FAILURE
+
+/* Macro para la re-asignación de memoria (filas/columnas) */
+#define realloc_m(typedata, matrixid, mode) if(_realloc_m(matrixid, sizeof(typedata), mode) == NULL) return EXIT_FAILURE
+
+/* Delete-matrix = Libera la memoria de una específica matriz */
+#define dmat_m(matrixid)													\
+for (uint32_t __i___g = 0; __i___g != dim[matrixid].new_rows; ++__i___g)	\
+free(ga(matrixid, __i___g));												\
+free(*(buf_ptr + matrixid));												\
+*(buf_ptr + matrixid) = NULL;												\
+dim[matrixid].new_rows = 0;													\
+dim[matrixid].old_rows = 0;													\
+dim[matrixid].cols = 0		
+
+/* Sirve para liberar la memoria de todas las matrices dinámicas que estén el heap/montón */
+#define free_m()															\
+for (uint8_t matrixid = 0; matrixid != MAX_MATRIX; ++matrixid)				\
+{																			\
+	if (*(buf_ptr + matrixid) != NULL)										\
+	{																		\
+		dmat_m(matrixid);													\
+	}																		\
 }
 
+/* Función para reservar memoria para filas y columnas */
+void* _alloc_m(const uint8_t matrixid, const uint8_t size)
+{
+	/* Reserva memoria para un búfer de punteros */
+	*(buf_ptr + matrixid) = (void**)malloc(dim[matrixid].new_rows * sizeof **(buf_ptr + matrixid));
+	if (*(buf_ptr + matrixid) == NULL)
+	{
+		puts(""MESSAGE_ALLOC_ROW"");
+		return NULL;
+	}
+	for (uint32_t i = 0; i != dim[matrixid].new_rows; ++i)
+	{
+		/* Reserva memoria para un búfer de X elementos donde cada elemento del búfer de punteros apuntará hacia el */
+		MOD_BUF_PTR = malloc(dim[matrixid].cols * size);
+		if (MOD_BUF_PTR == NULL)
+		{
+			puts(""MESSAGE_ALLOC_COL"");
+			dim[matrixid].new_rows = i;
+			return NULL;
+		}
+	}
+	dim[matrixid].old_rows = dim[matrixid].new_rows;
+	return *(buf_ptr + matrixid);
+}
 
-#define ALLOC_RC(matrixid, namearray, nameptr)																											\
-namearray[matrixid].nameptr = malloc(dim[matrixid].new_rows * sizeof namearray[matrixid].nameptr);														\
-if (namearray[matrixid].nameptr == NULL)																												\
-{																																						\
-	puts(""MESSAGE_ALLOC_ROW"");																														\
-	FREE_MEMORY_ALL(namearray, nameptr)																													\
-	return EXIT_FAILURE;																																\
-}																																						\
-for (it = 0; it != dim[matrixid].new_rows; ++it)																										\
-{																																						\
-	*(namearray[matrixid].nameptr + it) = malloc(dim[matrixid].cols * sizeof **namearray[matrixid].nameptr);											\
-	if (*(namearray[matrixid].nameptr + it) == NULL)																									\
-	{																																					\
-		puts(""MESSAGE_ALLOC_COL"");																													\
-		dim[matrixid].new_rows = it;																													\
-		DELETE_MATRIX(namearray[matrixid].nameptr, dim[matrixid].new_rows)																				\
-		FREE_MEMORY_ALL(namearray, nameptr)																												\
-		return EXIT_FAILURE;																															\
-	}																																					\
-}																																						\
-dim[matrixid].old_rows = dim[matrixid].new_rows;
+/*
+	Función que re-dimensiona una determinada matriz dinámica.
+	Puede re-dimensionar tres cosas:
+	- Filas y columnas. 
+	- Únicamente filas. 
+	- Únicamente columnas.
+*/
+void* _realloc_m(const uint8_t matrixid, const uint8_t size, const _mode mode)
+{
+	void* ptrtemp;
+	uint32_t i;
+	if (mode == ROWS || mode == ROWS_COLS)
+	{
+		if (dim[matrixid].new_rows < dim[matrixid].old_rows)
+		{
+			/* Caso 1: Código que sirve para liberar la memoria de las columnas que no se van a usar más. */
+			for (i = dim[matrixid].new_rows; i != dim[matrixid].old_rows; ++i)
+				free(MOD_BUF_PTR);
+			dim[matrixid].old_rows = dim[matrixid].new_rows;
+		}
+		ptrtemp = *(buf_ptr + matrixid);
+		/* Re-dimensiona el búfer de punteros de una matriz */
+		*(buf_ptr + matrixid) = (void**)realloc(ptrtemp, dim[matrixid].new_rows * sizeof **(buf_ptr + matrixid));
+		if (*(buf_ptr + matrixid) == NULL)
+		{
+			puts(""MESSAGE_REALLOC_ROW"");
+			if (ptrtemp != NULL)
+			{
+				*(buf_ptr + matrixid) = (void**)ptrtemp;
+				dim[matrixid].new_rows = dim[matrixid].old_rows;
+			}
+			RE_ASSIGN;
+			return NULL;
+		}
+		if (dim[matrixid].new_rows > dim[matrixid].old_rows)
+		{
+			/* Caso 2: Código que sirve para reservar memoria para las columnas faltantes */
+			for (i = dim[matrixid].old_rows; i != dim[matrixid].new_rows; ++i)
+			{
+				MOD_BUF_PTR = malloc(dim[matrixid].cols * size);
+				if (MOD_BUF_PTR == NULL)
+				{
+					puts(""MESSAGE_ALLOC_COL"");
+					dim[matrixid].new_rows = i;
+					dim[matrixid].old_rows = i;
+					RE_ASSIGN;
+					return NULL;
+				}
+			}
+		}
+	}
+	if (mode == COLS || mode == ROWS_COLS)
+	{
+		for (i = 0; i != dim[matrixid].old_rows; ++i)
+		{
+			ptrtemp = MOD_BUF_PTR;
+			/* Re-dimensiona el tamaño de columnas de dicha matriz */
+			MOD_BUF_PTR = realloc(ptrtemp, dim[matrixid].cols * size);
+			if (MOD_BUF_PTR == NULL)
+			{
+				MOD_BUF_PTR = ptrtemp;
+				puts(""MESSAGE_REALLOC_COL"");
+				dim[matrixid].old_rows = dim[matrixid].new_rows;
+				RE_ASSIGN;
+				return NULL;
+			}
+		}
+	}
+	dim[matrixid].old_rows = dim[matrixid].new_rows;
+	return *(buf_ptr + matrixid);
+}
 
+/* Hook/enganche a main */
 
-#define REALLOC_COLS(matrixid, namearray, nameptr)																										\
-for (it = 0; it != dim[matrixid].new_rows; ++it)																										\
-{																																						\
-	ptrtemp = *(namearray[matrixid].nameptr + it);																										\
-	*(namearray[matrixid].nameptr + it) = realloc(*(namearray[matrixid].nameptr + it), dim[matrixid].cols * sizeof **namearray[matrixid].nameptr);		\
-	if (*(namearray[matrixid].nameptr + it) == NULL)																									\
-	{																																					\
-		*(namearray[matrixid].nameptr + it) = ptrtemp;																									\
-		puts(""MESSAGE_REALLOC_COL"");																													\
-		DELETE_MATRIX(namearray[matrixid].nameptr, dim[matrixid].new_rows)																				\
-		FREE_MEMORY_ALL(namearray, nameptr)																												\
-		return EXIT_FAILURE;																															\
-	}																																					\
-}																						
+int mDynamic_main();
 
+int main(void)
+{
+	uint8_t xx = mDynamic_main();
+	free_m();
+	return xx;
+}
 
-#define REALLOC_ROWS(matrixid, namearray, nameptr)																										\
-if (dim[matrixid].new_rows < dim[matrixid].old_rows)																									\
-{																																						\
-	for (it = dim[matrixid].new_rows; it != dim[matrixid].old_rows; ++it)																				\
-		free(*(namearray[matrixid].nameptr + it));																										\
-}																																						\
-ptrtemp = namearray[matrixid].nameptr;																													\
-namearray[matrixid].nameptr = realloc(namearray[matrixid].nameptr, dim[matrixid].new_rows * sizeof namearray[matrixid].nameptr);						\
-if (namearray[matrixid].nameptr == NULL)																												\
-{																																						\
-	puts(""MESSAGE_REALLOC_ROW"");																														\
-	if(ptrtemp != NULL)																																	\
-	{																																					\
-		namearray[matrixid].nameptr = ptrtemp;																											\
-		DELETE_MATRIX(namearray[matrixid].nameptr, dim[matrixid].old_rows)																				\
-	}																																					\
-	FREE_MEMORY_ALL(namearray, nameptr)																													\
-	return EXIT_FAILURE;																																\
-}																																						\
-if (dim[matrixid].new_rows > dim[matrixid].old_rows)																									\
-{																																						\
-	for (it = dim[matrixid].old_rows; it != dim[matrixid].new_rows; ++it)																				\
-	{																																					\
-		*(namearray[matrixid].nameptr + it) = malloc(dim[matrixid].cols * sizeof **namearray[matrixid].nameptr);										\
-		if (*(namearray[matrixid].nameptr + it) == NULL)																								\
-		{																																				\
-			puts(""MESSAGE_ALLOC_COL"");																												\
-			dim[matrixid].new_rows = it;																												\
-			DELETE_MATRIX(namearray[matrixid].nameptr, dim[matrixid].new_rows)																			\
-			FREE_MEMORY_ALL(namearray, nameptr)																											\
-			return EXIT_FAILURE;																														\
-		}																																				\
-	}																																					\
-}																																						\
-dim[matrixid].old_rows = dim[matrixid].new_rows;
+#ifndef _ALS_main
+	#define _ALS_main
+#else
+	#undef main
+#endif
+#define main mDynamic_main
 
 #endif /* _INC_MDYNAMIC */
